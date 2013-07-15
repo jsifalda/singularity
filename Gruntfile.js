0f5462e9b1a2ce9dff6f9fe931ed7826817f33f1
JSifalda <sifalda.jiri@gmail.com>
2013-07-15 20:44:26 +0200

    Add layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,23 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   appScripts = [
     'www/bower_components/angular/angular.min.js',
     'www/js/app.js',
     'www/js/controllers/Countdown.js',
     'www/js/services/Remaining.js'
+  ];
+
+  cssminFiles = [
+    'www/bower_components/pure/src/base/css/normalize.css',
+    'www/bower_components/pure/src/base/css/normalize-context.css',
+    'www/bower_components/pure/src/buttons/css/buttons-core.css',
+    'www/bower_components/pure/src/buttons/css/buttons.css',
+    'www/bower_components/pure/src/grids/css/grids-core.css',
+    'www/bower_components/pure/src/grids/css/grids-r.css',
+    'www/bower_components/pure/src/grids/css/grids-units.css',
+    'www/css/core.css'
   ];
 
   grunt.initConfig({
@@ -20,8 +32,15 @@ module.exports = function(grunt) {
           'www/build/app.js': appScripts
         }
       }
+    },
+    cssmin: {
+      app: {
+        files: {
+          'www/build/core.css': cssminFiles
+        }
+      }
     }
   });
 
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
 }
